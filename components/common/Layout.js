43c5821672e633a48007d26e9e0d40c6a8f2cb47
“Andrey” <andrey.kallko@gmail.com>
2022-05-05 12:55:06 +0300

    burger-work-git-fix

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -7,6 +7,7 @@ export default function Layout({ children }) {
   return (
     <div className="wrapper">
       <Header />
+
       <main className="content">{children}</main>
       <Footer />
       <Burger />
