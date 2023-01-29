9ed1dda164117ac589c2a2aa7984224c5f61f95f
Andrij Kalko <andrey.kallko@gmail.com>
2023-01-29 20:27:55 +0200

    parse-url2

--- components/searchpage/pasteUrl/pasteUrl.js
+++ components/searchpage/pasteUrl/pasteUrl.js
@@ -1,5 +1,5 @@
 export default async function parseUrl(router) {
-  // ищем страну
+  // шукаємо країну
   const isCountry = await fetch(
     `https://api.otpusk.com/api/2.6/tours/countries?access_token=337da-65e22-26745-a251f-77b9e`
   ).then((response) => {
@@ -9,43 +9,78 @@ export default async function parseUrl(router) {
     return null;
   });
 
-  if (isCountry) {
-    console.log('isCountry', isCountry);
-    console.log('router.query.to', router.query.to);
-    let isCountrySearched = isCountry.countries.filter(
-      (item) => item.id === Number(router.query.to)
-    );
-    console.log('searched', isCountrySearched);
+  console.log('router.query.to', router.query.to);
+  console.log('isCountry', isCountry);
+  const isCountrySearched = isCountry.countries.filter(
+    (item) => item.id === Number(router.query.to)
+  );
+  console.log('searched', isCountrySearched);
 
-    if (!isCountrySearched.length) {
-      // это не страна, ищем дальше
-      const isDistrict = await fetch(
-        `https://api.otpusk.com/api/2.6/tours/geotree?id=${router.query.country}&depth=district&access_token=337da-65e22-26745-a251f-77b9e`
+  if (isCountrySearched.length === 1) {
+    // країну знайдено
+    console.log('країну знайдено', isCountrySearched);
+  } else if (isCountrySearched.length > 1) {
+    console.log('error');
+  } else {
+    // це не країна, шукаємо дістрікт
+    const isDistrict = await fetch(
+      `https://api.otpusk.com/api/2.6/tours/geotree?id=${router.query.country}&depth=district&access_token=337da-65e22-26745-a251f-77b9e`
+    ).then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      return null;
+    });
+
+    console.log('isDistrict', isDistrict);
+
+    const isDistrictSearched = isDistrict.geo.filter((item) => {
+      if (item.id === Number(router.query.to)) {
+        return item;
+      } else if (item.children && item.children.length) {
+        return item.children.map((child) => {
+          if (child.id === Number(router.query.to)) {
+            return child;
+          } else if (child.children && child.children.length) {
+            return child.children.map(
+              (distr) => distr === Number(router.query.to)
+            );
+          }
+        });
+      }
+    });
+    console.log('isDistrictSearched', new Set(isDistrictSearched.flat()));
+    console.log('isDistrictSearched', isDistrictSearched);
+    if (isDistrictSearched.length === 1) {
+      // дістрікт знайдено
+      console.log('дістрікт знайдено', isDistrictSearched);
+    } else if (isDistrictSearched.length > 1) {
+      console.log('error');
+    } else {
+      // це не країна і не дістрікт шукаємо готель
+      const isHotel = await fetch(
+        `https://api.otpusk.com/api/2.6/tours/hotels?countryId=${router.query.country}&access_token=337da-65e22-26745-a251f-77b9e`
       ).then((response) => {
         if (response.status === 200) {
           return response.json();
         }
         return null;
       });
+      console.log('isHotel', isHotel);
+      const isHotelSearched = isHotel.hotels.filter(
+        (item) => item.id === Number(router.query.to)
+      );
 
-      if (!isDistrict.geo.length) console.log('isDistrict', isDistrict);
+      if (isHotelSearched.length === 1) {
+        //готель знайдено
+        console.log('isHotelSearched', isHotelSearched);
+      } else if (isHotelSearched.length > 1) {
+        console.log('error');
+      } else {
+        console.log('error');
+      }
     }
   }
-  // const search = await fetch(
-  //   `/api/endpoints/fromcities?geoId=${down.value}`
-  // ).then((response) => {
-  //   if (response.status === 200) {
-  //     return response.json();
-  //   }
-  //   return null;
-  // });
-
-  // if (search?.ok) {
-  //   setUpPointList({
-  //     active: true,
-  //     list: search.result.fromCities,
-  //   });
-  // }
 
   // router.push({
   //   pathname: '/search',
