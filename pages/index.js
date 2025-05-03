8eec8d911c266f3c68afb11aad57d9e1f122bd7b
Andrij <andrey.kallko@gmail.com>
2025-05-03 17:38:54 +0300

    add min-offer

--- pages/index.js
+++ pages/index.js
@@ -26,10 +26,7 @@ export default function Home({
       <SeoHead content={mainPageSettings} />
       <div className="container">
         <MainForm />
-        {/* <PopularCountry
-          data={popularCountry}
-          minOffer={minOffer?.data?.countries}
-        /> */}
+        <PopularCountry data={popularCountry} minOffer={minOffer?.data?.countries} />
         <Blog data={postsList} />
         {faqData && <Faq data={faqData} length={faqDataLength} />}
         {mainPageSettings.translations && <SeoBlock text={mainPageSettings.translations[0].seo_block} />}
