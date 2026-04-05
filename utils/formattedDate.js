0934cb72691458722078d174df5ae247b1d27429
Andrij <andrey.kallko@gmail.com>
2026-04-05 22:47:21 +0300

    refactor(MultipleDatepicker): simplify date formatting logic and clean up CSS styles for switch menu and button

--- utils/formattedDate.js
+++ utils/formattedDate.js
@@ -1,9 +1,8 @@
 export function formattedDate(date) {
-  const year = date.getFullYear();
+  const year = String(date.getFullYear()).slice(-2);
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
   return `${day}.${month}.${year}`;
-  // return date.toISOString().slice(0, 10).split('-').reverse().join('.');
 }
 
 export function directusFormattedDate(date) {
